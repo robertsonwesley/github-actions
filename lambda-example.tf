fdcc4073bb32ae4c928cc53c8c05ff63377f7dc9
Wesley Robertson <adoptedmanatee@gmail.com>
2023-06-09 14:36:50 -0400

    Update lambda-example.tf

--- lambda-example.tf
+++ lambda-example.tf
@@ -1,16 +1,42 @@
-resource "aws_lambda_function" "test_lambda" {
-  function_name   = "${var.lambda_name}-${var.env_name}"
-  description     = "Lambda funtion created using terraform"
-  timeout         = var.timeout
-  package_type    = "zip"
-  runtime         = "xyz"
-  memory_size     = var.memory_size
-  role            = aws_iam_role.aws-lambda-role.arn
-  handler         = "index.test"
-  filename        = "test.zip"
-  vpc_config {
-    subnet_ids    = var.subnet_ids
-    security_group_ids = [aws_security_group.aws_sg.id]
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
   }
-  tags            = var.tags
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "lambda.js"
+  output_path = "lambda_function_payload.zip"
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = "lambda_function_payload.zip"
+  function_name = "lambda_function_name"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "index.test"
+
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  runtime = "xyz"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
 }
