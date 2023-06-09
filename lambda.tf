78df0ffe46d4b4b25a1e1504a9fc70eabb0f3fa5
Wesley Robertson <adoptedmanatee@gmail.com>
2023-06-09 14:21:13 -0400

    Create lambda.tf
    testing

---
+++ lambda.tf
@@ -0,0 +1,17 @@
+resource "aws_lambda_function" "billing_function" {
+function_name = "${var.lambda_name}-${var.env_name}"
+description = "Lambda funtion created using terraform"
+timeout = var.timeout
+package_type = "zip"
+runtime = "xyz"
+memory_size = var.memory_size
+role = aws_iam_role.aws-lambda-role.arn
+handler = "index.test"
+filename = "test.zip"
+vpc_config {
+subnet_ids = var.subnet_ids
+security_group_ids = [aws_security_group.aws_sg.id]
+}
+
+tags = var.tags
+}
